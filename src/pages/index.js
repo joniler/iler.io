07308e042eef1a63958d8d9176f91c814f1d10b8
Jon Iler <jon.iler@dfo.global>
2019-03-04 13:07:51 -0800

    query updates and additional blog page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,28 +2,23 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/Layout/DefaultLayout'
 import HomeAnimation from '../components/HomeAnimation/HomeAnimation'
+import Resume from '../components/Resume/Resume'
 
 const IndexPage = () => (
     <Layout>
         <Helmet
             title="Home"
             meta={[
-                { name: 'description', content: 'Sample' },
-                { name: 'keywords', content: 'sample, something' }
+                { name: 'iler.io Home Page', content: 'Interactive home animation' },
+                { name: 'keywords', content: 'home' }
             ]}
             bodyAttributes={{
                 class: 'page-index'
             }}
         />
-        {/* 
-            Couldn't find a way to render emoji consistently and accessibly
-            that worked across all browsers so disabling the eslint warning
-            for now.
-
-            I mean, you were going to erase this anyway...sheeeeesh!
-        */}
         {/* eslint-disable jsx-a11y/accessible-emoji */}
         <HomeAnimation/>
+        <Resume/>
     </Layout>
 )
 
