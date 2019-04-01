be03b50b9e5c6728e3d12e3db4b7c52e68c77f10
Jon Iler <jon.iler@dfo.global>
2019-04-01 14:12:39 -0700

    added UI detail and font stacks

--- src/components/OverlayContainer/OverlayContainer.js
+++ src/components/OverlayContainer/OverlayContainer.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-// import Me from '../Pages/Me/Me';
+import Me from '../Pages/Me/Me';
 import Work from '../Pages/Work/Work';
 import './OverlayContainer.scss';
 
 class OverlayContainer extends Component {
+    const 
     render() {
         return(
             <div className={"overlayContainer" + (this.props.active ? ' active' : '')}>
                 <Work/>
+                {/* <Me/> */}
             </div>
         )
     }
